e2cf8d5eeafc6fbe7f94215ba8d672867f60b216
kodai9263 <kou92638685@gmail.com>
2025-07-31 16:42:27 +0900

    Header修正

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import classes from './Header.module.css';
 export default function Header() {
   return (
     <header className={classes.header}>
-        <Link to="/" className={classes.headerLink}>Blog</Link>
-        <Link  className={classes.headerLink}>お問い合わせ</Link>
+      <Link to="/" className={classes.headerLink}>Blog</Link>
+      <Link  className={classes.headerLink}>お問い合わせ</Link>
     </header>
   );
 }
